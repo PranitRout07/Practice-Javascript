8b3fe57192a9424ce7a7b69f8aa7090237b25607
PranitRout07 <pranitrout72@gmail.com>
2024-07-27 19:28:54 +0530

    functions

---
+++ functions/11_functions.js
@@ -0,0 +1,13 @@
+// function sayMyName(username="pranit"){ //by default i am passing pranit as argument,if you call the function by giving another argument that argument will overwrite this default argument.
+//     // suppose here default is not taken then in this situation we have to go for checking the username is empty or not
+//     if (!username){
+//         console.log("Please enter a username")
+//         return 
+//     }
+//     return username
+// } 
+
+// name = sayMyName("joe")
+// console.log(name)
+
+//
